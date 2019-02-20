d0f4d1baf568fc5ec54b1f1d46b510ce0266af03
Gorrie Coe <gorriecoe@gmail.com>
2019-02-20 17:25:21 +1300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# Introduction
+
+Silverstripe Mix is a wrapper around Laravel Mix to simplify SS module and theme development.
+
+## Installation
+
+1. Run `npm install silvertripe-mix`.
+1. Add the following NPM scripts to your `package.json` file.
+
+```json
+"scripts": {
+    "dev": "npm run development",
+    "development": "cross-env NODE_ENV=development node_modules/webpack/bin/webpack.js --progress --hide-modules --config=node_modules/laravel-mix/setup/webpack.config.js",
+    "watch": "cross-env NODE_ENV=development node_modules/webpack/bin/webpack.js --watch --progress --hide-modules --config=node_modules/laravel-mix/setup/webpack.config.js",
+    "hot": "cross-env NODE_ENV=development webpack-dev-server --inline --hot --config=node_modules/laravel-mix/setup/webpack.config.js",
+    "prod": "npm run production",
+    "production": "cross-env NODE_ENV=production node_modules/webpack/bin/webpack.js --progress --hide-modules --config=node_modules/laravel-mix/setup/webpack.config.js"
+}
+```
+
+1. Create a `webpack.mix.js` NOT `webpack.config.js` file, with the following:
+
+```js
+const mix = require('silverstripe-mix')
+```
+
+1. Refer to [documentation](#documentation) for additional required configuration.
+
+## Documentation
+
+You may review the initial documentation [here](https://laravel-mix.com/docs/4.0/basic-example).
+
+## License
+
+Laravel Mix is open-sourced software licensed under the [BSD license](http://opensource.org/licenses/MIThttps://opensource.org/licenses/BSD-3-Clause)
---
+++ src/index.js
@@ -0,0 +1,85 @@
+const mix = require('laravel-mix')
+
+const {
+    resolveJS,
+    externalJS,
+    moduleJS,
+    pluginJS,
+    moduleCSS,
+    pluginCSS,
+} = require('@silverstripe/webpack-config')
+
+mix.setPublicPath('client/dist')
+mix.setResourceRoot('client/src')
+
+// mix.webpackConfig({
+//     devtool: (ENV !== 'production') ? 'source-map' : '',
+//     resolve: resolveJS(ENV, PATHS),
+//     externals: externalJS(ENV, PATHS),
+//     module: moduleJS(ENV, PATHS),
+//     plugins: pluginJS(ENV, PATHS)
+// })
+
+// export default mix
+
+module.exports = mix
+
+// const Path = require('path');
+// const webpack = require('webpack');
+// // Import the core config
+// const webpackConfig = require('@silverstripe/webpack-config');
+// const {
+//   resolveJS,
+//   externalJS,
+//   moduleJS,
+//   pluginJS,
+//   moduleCSS,
+//   pluginCSS,
+// } = webpackConfig;
+
+// const ENV = process.env.NODE_ENV;
+// const PATHS = {
+//   // the root path, where your webpack.config.js is located.
+//   ROOT: Path.resolve(),
+//   // your node_modules folder name, or full path
+//   MODULES: 'node_modules',
+//   // relative path from your css files to your other files, such as images and fonts
+//   FILES_PATH: '../',
+//   // thirdparty folder containing copies of packages which wouldn't be available on NPM
+//   THIRDPARTY: 'thirdparty',
+//   // the root path to your javascript source files
+//   SRC: Path.resolve('client/src'),
+// };
+
+// const config = [
+//   {
+//     name: 'js',
+//     entry: {
+//       main: 'js/src/main.js'
+//     },
+//     output: {
+//       path: 'js/dist',
+//       filename: '[name].bundle.js',
+//     },
+//     devtool: (ENV !== 'production') ? 'source-map' : '',
+//     resolve: resolveJS(ENV, PATHS),
+//     externals: externalJS(ENV, PATHS),
+//     module: moduleJS(ENV, PATHS),
+//     plugins: pluginJS(ENV, PATHS),
+//   },
+//   {
+//     name: 'css',
+//     entry: {
+//       main: 'css/src/main.scss'
+//     },
+//     output: {
+//       path: 'css/dist',
+//       filename: '[name].css'
+//     },
+//     devtool: (ENV !== 'production') ? 'source-map' : '',
+//     module: moduleCSS(ENV, PATHS),
+//     plugins: pluginCSS(ENV, PATHS),
+//   },
+// ];
+
+// module.exports = config;
